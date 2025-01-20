a07a7d744e1e30a8e5e36b8e26454383ccb6ffc4
EAG <franreke506@gmail.com>
2025-01-20 16:29:25 +0100

    siuuuu

---
+++ main.py
@@ -0,0 +1,4 @@
+'''Ejercicio 1: Comparación de Productos
+Crea una clase Producto que represente un artículo en una tienda. Esta clase
+debe tener los atributos nombre, precio y codigo. Sobrecarga el método
+__eq__ para considerar dos productos iguales si tienen el mismo código.'''
